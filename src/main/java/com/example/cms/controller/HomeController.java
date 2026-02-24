e4dc7a0dde3019341723125d3969701117170352
Saraswathy <ushagopalakrishnan078@gmail.com>
2026-02-24 07:09:22 +0530

    Initial commit - Spring Boot CMS setup

---
+++ src/main/java/com/example/cms/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.cms.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "College Management System Running Successfully";
+    }
+}
